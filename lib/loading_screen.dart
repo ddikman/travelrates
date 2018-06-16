b90ff131b38285c389c5cfb58da648991f058550
David Dikman <ddikman@gmail.com>
2018-06-16 21:45:47 +0800

    ^

--- lib/loading_screen.dart
+++ lib/loading_screen.dart
@@ -1,31 +1,39 @@
-
 import 'dart:async';
 
-import 'package:backpacking_currency_converter/animate_in.dart';
 import 'package:backpacking_currency_converter/background_container.dart';
 import 'package:backpacking_currency_converter/state_container.dart';
 import 'package:flutter/material.dart';
 
 class LoadingScreen extends StatefulWidget {
-
   @override
   _LoadingScreenState createState() {
     return new _LoadingScreenState();
   }
-
 }
 
-class _LoadingScreenState extends State<LoadingScreen> {
+class _LoadingScreenState extends State<LoadingScreen>
+    with TickerProviderStateMixin {
+  AnimationController _animationController;
+
+  Animation<double> _opacity;
 
   @override
   void initState() {
+    _animationController = new AnimationController(
+        vsync: this, duration: Duration(milliseconds: 600));
+
+    _opacity = Tween<double>(begin: 0.0, end: 1.0).animate(new CurvedAnimation(
+        parent: _animationController, curve: Curves.easeInOut));
+
+    _animationController.forward();
     _loadState();
     super.initState();
   }
 
   Future<Null> _loadState() async {
     await StateContainer.of(context).loadState();
-    Navigator.pushReplacementNamed(context, '/home');
+    await _animationController.reverse();
+    Navigator.of(context).pushReplacementNamed('/home');
   }
 
   @override
@@ -33,10 +41,10 @@ class _LoadingScreenState extends State<LoadingScreen> {
     return new BackgroundContainer(
       child: new Center(
           child: new Container(
-            width: 120.0,
-            height: 120.0,
-            child: _animated(_loader()),
-          )),
+        width: 120.0,
+        height: 120.0,
+        child: _animation(_loader()),
+      )),
     );
   }
 
@@ -49,7 +57,10 @@ class _LoadingScreenState extends State<LoadingScreen> {
     );
   }
 
-  Widget _animated(Widget child) {
-    return new AnimateIn(child: child, move: false);
+  _animation(Widget child) {
+    return new FadeTransition(
+      opacity: _opacity,
+      child: child,
+    );
   }
 }
