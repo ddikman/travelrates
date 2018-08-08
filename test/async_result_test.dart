f7fa711cb6f6b7ba075a9a697270902081079f6c
ddikman@gmail.com <8LQ705*CoG9h>
2018-08-08 23:44:16 +0200

    Added tests.

---
+++ test/async_result_test.dart
@@ -0,0 +1,15 @@
+import 'package:backpacking_currency_converter/model/async_result.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('when getting result of failed async operation, throws state error', () {
+    final failed = AsyncResult<String>.failed();
+    expect(() => failed.result, throwsA(isInstanceOf<StateError>()));
+  });
+
+  test('can get result on successful result', () {
+    final success = AsyncResult.withValue('success!');
+    expect(success.result, 'success!');
+    expect(success.successful, true);
+  });
+}
