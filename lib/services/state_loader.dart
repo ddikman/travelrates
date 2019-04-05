457db567f59d8ad9aefb50677b5dfeef68b3574c
David Dikman <ddikman@gmail.com>
2019-04-05 21:53:13 +0100

    Removed loads of unused classes.

--- lib/services/state_loader.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:async';
-
-import 'package:flutter/widgets.dart';
-
-/// Binds together the logic of loading the app state
-abstract class StateLoader {
-
-  Future load(BuildContext context);
-}
