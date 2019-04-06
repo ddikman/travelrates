be8c5121fe22d94045e238d669b4b646c23be800
David Dikman <ddikman@gmail.com>
2019-04-06 21:36:14 +0100

    Additional test for rates download.

--- test/rates_loader_test.dart
+++ test/rates_loader_test.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:travelconverter/model/async_result.dart';
+import 'package:travelconverter/services/rates_api.dart';
 import 'package:travelconverter/services/rates_loader.dart';
 
 import 'mocks/mock_bundle.dart';
@@ -9,11 +11,51 @@ void main() {
   final ratesJson = "ratesjson";
   localStorage.setFile("rates.json", ratesJson);
 
+  final ratesApi = new MockRatesApi();
+
   final assets = new TestAssetBundle();
 
+  final ratesLoader = new RatesLoader(localStorage: localStorage, ratesApi: ratesApi);
+
   test('uses cached rates if existing', () async {
-    final ratesLoader = new RatesLoader(localStorage: localStorage);
     final rates = await ratesLoader.load(assets);
     expect(rates, ratesJson);
   });
+
+  test('returns false result when api fails', () async {
+    ratesApi.result = new AsyncResult.failed();
+
+    final rates = await ratesLoader.loadOnlineRates();
+    expect(rates.successful, false);
+    expect(() => rates.result, throwsA(isInstanceOf<StateError>()));
+  });
+
+  test('decodes api json', () async {
+    final apiJson = '{"rates":{"EUR":1}}';
+    ratesApi.result = new AsyncResult.withValue(apiJson);
+
+    final rates = await ratesLoader.loadOnlineRates();
+    expect(rates.successful, true);
+    expect(rates.result.length, 1);
+
+    final currency = rates.result.first;
+    expect(currency.currencyCode, 'EUR');
+    expect(currency.rate, 1.0);
+  });
+
+  test('returns failure on invalid api call', () async {
+    ratesApi.result = new AsyncResult.withValue("invalid json");
+    await expectLater(() => ratesLoader.loadOnlineRates(), throwsA(isInstanceOf<FormatException>()));
+  });
+}
+
+class MockRatesApi implements RatesApi {
+
+  AsyncResult<String> result;
+
+  @override
+  Future<AsyncResult<String>> getCurrentRatesJson() {
+    return Future.value(result);
+  }
+
 }
