96a17963114d632c4aed5815ba586c55bbe4ef80
David Dikman <ddikman@gmail.com>
2019-06-14 00:25:50 +0100

    Updated helper.

--- tools/read-machine.js
+++ tools/read-machine.js
@@ -9,6 +9,13 @@ var tests = raw.filter(item => item.test && item.test.url !== null).map(item => 
 tests.forEach(t => {
     t.result = raw.find(r => r.testID === t.id && r.type === 'testDone').result;
     t.prints = raw.filter(r => r.testID === t.id && r.messageType === 'print').map(p => p.message);
+    var error = raw.find(r => r.error && r.testID === t.id);
+    if (error) {
+        t.error = {
+            message: error.error,
+            trace: error.stackTrace.replace('\\n', '\n')
+        }
+   }
 });
 
 var failures = tests.filter(t => t.result !== 'success');
@@ -20,5 +27,9 @@ if (failures.length === 0) {
         console.log(`[${f.result}]: ${f.name}`);
         f.prints.forEach(p => console.log(`${p}`));
         console.log();
-    })
+        console.log(`error:  ${f.error.message}`);
+        console.log(`trace:`);
+        console.log(f.error.trace);
+    });
+    process.exit(1);
 }
