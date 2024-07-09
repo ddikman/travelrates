47061b35ac7256d5754252b8fe3b1f5c663cba2a
David Dikman <ddikman@gmail.com>
2024-07-09 14:15:24 +0200

    Removed unused edit mode references

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -3,23 +3,17 @@ import 'package:travelconverter/model/country.dart';
 import 'package:travelconverter/services/currency_repository.dart';
 
 class AppState {
-  final _AppMode _mode;
-
   final ConversionModel conversion;
 
   final CurrencyRepository availableCurrencies;
 
   final List<Country> countries;
 
-  bool get isEditing => _mode == _AppMode.editing;
-
   const AppState(
-      {required _AppMode mode,
-      required ConversionModel conversion,
+      {required ConversionModel conversion,
       required CurrencyRepository availableCurrencies,
       required List<Country> countries})
-      : this._mode = mode,
-        this.conversion = conversion,
+      : this.conversion = conversion,
         this.countries = countries,
         this.availableCurrencies = availableCurrencies;
 
@@ -27,7 +21,6 @@ class AppState {
     return new AppState(
         conversion: conversion,
         availableCurrencies: this.availableCurrencies,
-        mode: this._mode,
         countries: this.countries);
   }
 
@@ -40,40 +33,19 @@ class AppState {
   AppState.fromJson(Map<String, dynamic> json, CurrencyRepository repository,
       List<Country> countries)
       : this.availableCurrencies = repository,
-        this._mode = _AppMode.ready,
         this.conversion = new ConversionModel(
             currentAmount: json['currentAmount'],
             currentCurrency: repository.getByCode(json['currentCurrency']),
             currencies: List.castFrom(json['currencies'])),
         this.countries = countries;
 
-  AppState withMode(_AppMode mode) {
-    return new AppState(
-        conversion: this.conversion,
-        availableCurrencies: this.availableCurrencies,
-        countries: this.countries,
-        mode: mode);
-  }
-
-  AppState startEdit() {
-    return this.withMode(_AppMode.editing);
-  }
-
-  AppState stopEdit() {
-    return this.withMode(_AppMode.ready);
-  }
-
   AppState.initial(
       {required List<Country> countries,
       required CurrencyRepository availableCurrencies})
       : this.availableCurrencies = availableCurrencies,
-        this._mode = _AppMode.ready,
         this.countries = countries,
         this.conversion = new ConversionModel(
             currentAmount: 1.0,
             currentCurrency: availableCurrencies.baseCurrency,
             currencies: <String>[]);
 }
-
-/// Specifies the applications current mode
-enum _AppMode { editing, ready }
